f1871e47cbf85e878a4e83da391e66bd924263cb
Nathan Ringo <remexre@gmail.com>
2015-11-25 19:47:43 -0600

    Adds logout.

--- ticketHandler.go
+++ ticketHandler.go
@@ -15,17 +15,15 @@ func ticketHandler(w http.ResponseWriter, r *http.Request) {
 	u := user.Current(c)
 	if u == nil {
 		url, err := user.LoginURL(c, r.URL.String())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Location", url)
-		w.WriteHeader(http.StatusFound)
+		if checkError(w, err) { return }
+		http.Redirect(w, r, url, http.StatusFound)
 		return
 	}
+	logoutURL, err := user.LogoutURL(c, r.URL.String())
+	if checkError(w, err) { return }
 	checkError(w, ticketTemplate.Execute(w, struct {
-		User string
+		LogoutURL string
 	}{
-		User: u.String(),
+		LogoutURL: logoutURL,
 	}))
 }
